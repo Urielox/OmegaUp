9f6736b650043db3efe9097d8148482c29e4937c
Urielox <43377310+Urielox@users.noreply.github.com>
2019-02-13 14:16:39 -0600

    Add files via upload

---
+++ 1VALORABSOLUTO.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long int a,b,c,d,r;
+int N;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> a >> b;
+    cout << abs(a+b);
+
+    return 0;
+}
