b6517ff78d91869de7e6a05bfc641a1c3660e9ae
Urielox <43377310+Urielox@users.noreply.github.com>
2019-03-20 07:42:14 -0600

    Update BISIESTO sencillo.cpp

--- BISIESTO sencillo.cpp
+++ BISIESTO sencillo.cpp
@@ -6,7 +6,9 @@ char chairo;
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    cin >> N;
+    while(cin >> N){
+    
+    
     if((N%4==0 && N%100!=0) || (N%100 == 0 && N%400 == 0)) {
         cout << 'S' << '\n';
     } else {
